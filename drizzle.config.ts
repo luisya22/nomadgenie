2c643599031530d7718aadaf3934b0375ec8c42b
Luis Matos <lmatosgarcia2@gmail.com>
2025-07-10 09:57:09 -0400

    chore: change deployment configuration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     out: "./src/db/migrations",
     dialect: "postgresql",
     dbCredentials: {
-        url: process.env.DATABASE_URL!
-    }
+        url: process.env.DATABASE_URL!,
+        ssl: 'prefer', 
+    },
 });
