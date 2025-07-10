0bfbe851b32659673ddb0b7b320412bba6015bc7
Luis Matos <lmatosgarcia2@gmail.com>
2025-07-10 15:54:01 -0400

    fix: not used page removed components

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,14 +1,10 @@
 "use client"
 
-import Itinerary from "@/components/trips/Itinerary";
-import TripBuilder from "@/components/trips/TripBuilderPages";
 
 
 export default function AppPage() {
     return (
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 mt-10">
-            <Itinerary/> 
-            <TripBuilder/>
         </div>
     );
 }
